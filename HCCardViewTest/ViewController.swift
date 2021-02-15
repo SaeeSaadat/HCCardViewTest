e112fa63d214129d7e72d10bd7e25f714fd4a5f5
saee <saee_saadat@outlook.com>
2021-02-15 22:58:47 +0330

    rest of the cards

--- HCCardViewTest/ViewController.swift
+++ HCCardViewTest/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        setupCards()
+        setupCards()
         setupCollectionView()
         
     }
